97b8d1c896cafe7bb0c1457fbf85225976d8d4bd
Alexander <itskilobyte@gmail.com>
2024-11-23 11:25:30 +0500

    Добавьте файлы проекта.

---
+++ AlgorithmsVisualisation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AlgorithmsVisualisation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
