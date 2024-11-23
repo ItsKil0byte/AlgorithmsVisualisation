005cfce6c7de6fdf16550a75a190d721a3a8bd99
Alexander <itskilobyte@gmail.com>
2024-11-23 17:08:07 +0500

    Added a template for sorting algorithms works.

--- AlgorithmsVisualisation/MainWindow.xaml.cs
+++ AlgorithmsVisualisation/MainWindow.xaml.cs
@@ -18,7 +18,11 @@ namespace AlgorithmsVisualisation
     {
         private List<int> array = [];
         private readonly Random random = new();
-        private readonly int sampleCount = 150;
+        private readonly int sampleCount = 150; // NOTE: Добавить выбор в программе.
+        private readonly int delay = 5; // NOTE: Добавить выбор в программе.
+
+        private CancellationTokenSource? cancellationTokenSource;
+        private bool isWorking = false;
 
         public MainWindow()
         {
@@ -48,6 +52,27 @@ namespace AlgorithmsVisualisation
             }
         }
 
+        // NOTE: Это просто заглушечка, но работает нормально.
+        // Попытаюсь все алгосы вынести как отдельный класс.
+        private async Task BubbleSort(Canvas canvas, List<int> array, int delay, CancellationToken token)
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j <  array.Count - 1 - i; j++)
+                {
+                    if (token.IsCancellationRequested) return;
+
+                    if (array[j] > array[j + 1])
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+
+                        DrawSamples(canvas, array);
+                        await Task.Delay(delay, token);
+                    }
+                }
+            }
+        }
+
         private void Shuffle(List<int> array)
         {
             for (int i = array.Count - 1; i > 0; i--)
@@ -59,11 +84,45 @@ namespace AlgorithmsVisualisation
 
         private void ShuffleButton_Click(object sender, RoutedEventArgs e)
         {
-            Shuffle(array);
-            DrawSamples(AlgCanvas, array);
+            if (!isWorking)
+            {
+                Shuffle(array);
+                DrawSamples(AlgCanvas, array);
+            }
         }
 
-        private void StartButton_Click(object sender, RoutedEventArgs e)
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isWorking)
+            {
+                cancellationTokenSource?.Cancel();
+                isWorking = false;
+                StartButton.Content = "Запустить";
+            }
+            else
+            {
+                cancellationTokenSource = new();
+                isWorking = true;
+                StartButton.Content = "Остановить";
+
+                try
+                {
+                    await BubbleSort(AlgCanvas, array, delay, cancellationTokenSource.Token);
+                }
+                catch (TaskCanceledException)
+                {
+                    // IGNORE
+                }
+                finally
+                {
+                    isWorking = false;
+                    StartButton.Content = "Запустить";
+                    cancellationTokenSource.Dispose();
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             array = Enumerable.Range(1, sampleCount).ToList();
             DrawSamples(AlgCanvas, array);
