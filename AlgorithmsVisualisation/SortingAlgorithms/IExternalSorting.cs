76e0f45fd6c51a993b6b5592baebd7477c85987f
sadtowa <toni.afanasev.0505@mail.ru>
2024-11-27 18:13:43 +0500

    (T _ T)

---
+++ AlgorithmsVisualisation/SortingAlgorithms/IExternalSorting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AlgorithmsVisualisation.SortingAlgorithms
+{
+    public interface IExternalSorting
+    {
+        Task Sort(
+            string inputFilePath,
+            int key,
+            CancellationToken token,
+            Action<string, string> addToColumn,
+            Action<string, string> removeFromColumn,
+            Func<CancellationToken, Task> onDynamicDelay,
+            Action<string> onLog,
+            Action<string> onExplain
+            //<int, int> onHighlight
+        );
+    }
+}
